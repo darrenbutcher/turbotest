053906ae74da584e742b34e8a46cc39fdb013159
Darren Butcher <dbutcher6@gmail.com>
2022-07-09 21:42:35 +0000

    feat: another change

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -3,7 +3,7 @@ import { Button } from "@darrenbutcher/ui";
 export default function Web() {
   return (
     <div>
-      <h2>Web Page!</h2>
+      <h2>Web Page! Now</h2>
       <Button />
       <p>Some text goes here</p>
       <p>New Text</p>
