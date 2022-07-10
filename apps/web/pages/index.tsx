751e067e93d26943604e4431257023ba910101d6
Darren Butcher <dbutcher6@gmail.com>
2022-07-10 16:31:30 +0000

    feat: add new feature

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -3,7 +3,7 @@ import { Button } from "@darrenbutcher/ui";
 export default function Web() {
   return (
     <div>
-      <h2>Web Page! Now :) THree</h2>
+      <h2>Web Page! Now :) THree HOW ARE YOU</h2>
       <Button />
       <p>Some text goes here</p>
       <p>New Text</p>
