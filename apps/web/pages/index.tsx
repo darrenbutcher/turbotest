3544e3276a15575ac4b9b73b03881b350e4c88f6
Darren Butcher <dbutcher6@gmail.com>
2022-07-10 20:20:40 +0000

    fix(web): fix the text

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -7,7 +7,7 @@ export default function Web() {
       <Button />
       <p>Some text goes here</p>
       <p>New Text</p>
-      <p>Thak you for using this app.</p>
+      <p>Thank you for using this app.</p>
     </div>
   );
 }
