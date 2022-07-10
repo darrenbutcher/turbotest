8cff4cfe48ee81abc5d343f83ca930b4e42a2dda
Darren Butcher <dbutcher6@gmail.com>
2022-07-09 20:52:17 -0400

    feat/f3 (#11)
    * feat: update web page
    
    * bump web

---
+++ .changeset/twenty-ways-march.md
@@ -0,0 +1,5 @@
+---
+'@darrenbutcher/turbotest-web': minor
+---
+
+Bump to 0.6.0
--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -3,7 +3,7 @@ import { Button } from "@darrenbutcher/ui";
 export default function Web() {
   return (
     <div>
-      <h2>Web Page! Now :) TWO</h2>
+      <h2>Web Page! Now :) THree</h2>
       <Button />
       <p>Some text goes here</p>
       <p>New Text</p>
