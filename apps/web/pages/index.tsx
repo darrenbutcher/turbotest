4f320cc892d26e0e6ef7629c9c7876fee0b27bab
Darren Butcher <dbutcher6@gmail.com>
2022-07-18 03:57:15 +0000

    feat: add authentication

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,10 +1,55 @@
 import { Button } from "@darrenbutcher/ui";
+// import { useFeature, IfFeatureEnabled } from "@growthbook/growthbook-react"
+// import flagsmith from "flagsmith";
+// import { useFlags, useFlagsmith } from 'flagsmith/react';
+import { useVariableValue } from "@devcycle/devcycle-react-sdk"
+
+// const IfFeatureEnabled = ({children, feature} :any) => {
+//   const flagsmith = useFlagsmith() 
+//   if (!flagsmith.hasFeature(feature)) {
+//     return null
+//   }
+//   else {
+//     return (
+//       <>
+//        {children}
+//       </>
+//     )
+//   }
+// }
+
+const IfFeatureEnabled = ({children, feature} :any) => {
+  if (!feature) {
+    return null
+  }
+  else {
+    return (
+      <>
+       {children}
+      </>
+    )
+  }
+}
 
 export default function Web() {
+  const uiButtonFeature = useVariableValue('ui-button', false)
+
   return (
     <div>
-      <h2>Welcome to WEB</h2>
-      <Button />
+      <h2>Welcome to WEB 2</h2>
+      {/* <IfFeatureEnabled feature='ui_btn'>
+        <Button/>
+      </IfFeatureEnabled>   */}
+      {/* <IfFeatureEnabled feature="ui-btn">
+        <Button/>
+      </IfFeatureEnabled> */}
+      {/* <div>
+        {featureVariable ? <Button/> : null}
+      </div> */}
+      <IfFeatureEnabled feature={uiButtonFeature}>
+        <Button />
+      </IfFeatureEnabled>
+
       <p>Some text goes here</p>
       <p>New Text</p>
       <p>Thank you for using this app.</p>
